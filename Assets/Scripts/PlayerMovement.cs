16d7c0fd9cf10b66792f2ec658ae1949d6c0db35
LCADNW <noahwade@lcad.edu>
2025-09-08 17:28:20 -0700

    Added WASD movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int playerSpeed = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("bingus");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            {
+            transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * playerSpeed * Time.deltaTime);
+        }
+        //whenever Player uses WASD, they move in that direction at PlayerSpeed 5. PlayerSpeed is a public, changeable number.
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.transform.Translate(Vector3.up * playerSpeed * Time.deltaTime);
+        }
+
+
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
