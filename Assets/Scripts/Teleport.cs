8896ac68a9015f65ba89d483e3d64fab5ae16216
LCADNW <noahwade@lcad.edu>
2025-09-14 23:07:07 -0700

    Teleport and FadeOut

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teleport : MonoBehaviour
+{
+
+   public GameObject teleportLocation;
+   public Image panel;
+    bool isAlreadyTeleporting = false;
+
+
+   public void Warp()
+    {
+       if (isAlreadyTeleporting) return;
+        StartCoroutine(WarpFade());
+            
+
+
+
+    }
+
+   public IEnumerator WarpFade()
+    {
+        isAlreadyTeleporting=true;
+        Color fade = Color.black;
+        fade.a = 0;
+        float increment = 0;
+        while (increment < 0.9f)
+        {
+            increment += 0.05f;
+            yield return new WaitForSeconds(0.05f);
+            fade.a = increment;
+            panel.color = fade;
+
+
+        }
+
+    
+        
+        
+        
+        PlayerSingleton.Instance.GetComponent<Transform>().position = teleportLocation.transform.position;
+
+        while (increment > 0.1f)
+        {
+            increment -= 0.05f;
+            yield return new WaitForSeconds(0.05f);
+            fade.a = increment;
+            panel.color = fade;
+
+
+        }
+
+    }
+}
